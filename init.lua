863b0a8abecbbd59fc7c92d30c667a8deddd3d74
Maherilaza <maherilaza@gmail.com>
2025-06-01 19:11:37 +0300

    update(sum change)

--- init.lua
+++ init.lua
@@ -94,3 +94,13 @@ vim.api.nvim_create_autocmd("InsertLeave", {
   pattern = "*",
   command = "set guicursor=n-v-c:block,i:block"
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "c",
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.expandtab = true
+  end,
+})
+
